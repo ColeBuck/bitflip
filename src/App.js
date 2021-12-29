e4befec389d29725cef46ba8869dd55bfdb1d7e5
Cole Buck <67663402+ColeBuck@users.noreply.github.com>
2021-12-29 10:25:26 -0500

    add basic game features

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ class App extends React.Component {
 
   constructor() {
     super();
+    var startingDifficulty = "Easy"
     this.state = {
       bits: [],
-      decimal: 0
+      decimal: 0,
+      difficulty: startingDifficulty,
+      goal: this.generateGoal(startingDifficulty),
+      score: 0
     };
   }
 
@@ -45,14 +49,60 @@ class App extends React.Component {
         temp += Math.pow(2, i)
       }
     }
+
     this.setState({
       decimal: temp
+    }, this.checkForGoal);
+  }
+
+  checkForGoal() {
+    if(this.state.decimal == this.state.goal) {
+      var newGoal = this.generateGoal(this.state.difficulty)
+      // re-generate new goal until it's different than the current decimal value
+      while(newGoal == this.state.decimal) {
+        newGoal = this.generateGoal(this.state.difficulty)
+      }
+
+      this.setState({
+        goal: newGoal,
+        score: this.state.score + 1
+      });
+    }
+  }
+
+  generateGoal(difficulty) {
+    switch(difficulty) {
+      case "Easy":
+        return Math.floor(Math.random() * 16)
+      case "Medium":
+        return Math.floor(Math.random() * 32) + 16
+      case "Hard":
+        return Math.floor(Math.random() * 64) + 48
+      case "Super Hard":
+        return Math.floor(Math.random() * 2048) + 2048
+    }
+  }
+
+  changeDifficulty() {
+    this.setState({
+      difficulty: document.getElementById("difficulty-select").value,
+      goal: this.generateGoal(document.getElementById("difficulty-select").value)
     });
   }
 
   render() {
     return <div className="App">
       <h1>BitFlip</h1>
+
+      <label for="difficulty-select">Difficulty: </label>
+      <select id="difficulty-select" onChange={() => this.changeDifficulty()}>
+        <option value="Easy">Easy</option>
+        <option value="Medium">Medium</option>
+        <option value="Hard">Hard</option>
+        <option value="Super Hard">Super Hard</option>
+      </select>
+
+      <p>Current Bits: {this.state.bits.length}</p>
 
       <div class="nav-row">
         <button onClick={() => this.addBits(1)}>Add Bit</button>
@@ -64,8 +114,9 @@ class App extends React.Component {
         <button onClick={() => this.removeBits(8)}>Remove 8 Bits</button>
       </div>
 
-      <p>Current Bits: {this.state.bits.length}</p>
       <p>Decimal = {this.state.decimal}</p>
+      <p>Goal: {this.state.goal}</p>
+      <h3>Score: {this.state.score}</h3>
 
       {this.state.bits.map((bit, index) => <Bit index={index} on={bit} flipBit={this.flipBit.bind(this)} />)}
     </div>
